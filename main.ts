c2cbad23f889b896fa277066573c7f0ba2731ab2
Andrew McClenaghan <amcclenaghan@atlassian.com>
2022-10-29 14:56:31 +1000

    Initial Working Plugin

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Vault, MetadataCache, TFile } from 'obsidian';
+import { CustomConfluenceClient } from './MyBaseClient';
+import * as confluence from 'showdown-confluence'
 
 interface MyPluginSettings {
-	mySetting: string;
+	confluenceBaseUrl: string;
+	confluenceParentId: string;
+	atlassianUserName: string;
+	atlassianApiToken: string;
+	folderToPublish: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	confluenceBaseUrl: '',
+	confluenceParentId: '',
+	atlassianUserName: '',
+	atlassianApiToken: '',
+	folderToPublish: 'Confluence Pages',
 }
 
 export default class MyPlugin extends Plugin {
@@ -16,66 +24,20 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			name: 'Publish to Confluence',
+			callback: async () => {
+				//new SampleModal(this.app).open();
+				const { vault, workspace, metadataCache } = this.app;
+				const publisher = new Publisher(vault, metadataCache, this.settings)
+				await publisher.doPublish();
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,19 +53,112 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class Publisher {
+
+	vault: Vault;
+	metadataCache: MetadataCache;
+
+	settings: MyPluginSettings;
+	confluenceClient: CustomConfluenceClient;
+	frontmatterRegex: RegExp = /^\s*?---\n([\s\S]*?)\n---/g;
+
+	constructor(vault: Vault, metadataCache: MetadataCache, settings: MyPluginSettings) {
+		this.vault = vault;
+		this.metadataCache = metadataCache;
+		this.settings = settings;
+
+		this.confluenceClient = new CustomConfluenceClient({
+			host: 'https://hello.atlassian.net',
+			authentication: {
+				basic: {
+					email: this.settings.atlassianUserName,
+					apiToken: this.settings.atlassianApiToken,
+				},
+			},
+		});
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+
+	async getFilesMarkedForPublishing(): Promise<TFile[]> {
+		const files = this.vault.getMarkdownFiles();
+		const filesToPublish = [];
+		for (const file of files) {
+			try {
+				const frontMatter = this.metadataCache.getCache(file.path).frontmatter
+				if (file.basename === "Test Note - Frontmatter opted out")
+					{
+						console.log({frontMatter})
+					}
+				if (
+					(file.path.startsWith(this.settings.folderToPublish) && (!frontMatter || frontMatter["connie-publish"] !== false))
+					|| (frontMatter && frontMatter["connie-publish"] === true)
+					) {
+					console.log({file});
+					filesToPublish.push(file);
+				}
+			} catch {
+				//ignore
+			}
+		}
+
+		return filesToPublish;
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	async doPublish() {
+
+		const parentPage = await this.confluenceClient.content.getContentById({id: this.settings.confluenceParentId, expand: ["body.storage", "space"]});
+		const spaceToPublishTo = parentPage.space;
+		console.log({parentPage});
+
+		const files = await this.getFilesMarkedForPublishing()
+		console.log({filesToPublish: files});
+
+		const adrFileTasks = files.map(file => this.publishFile(file, spaceToPublishTo.key, parentPage.id))
+
+		const adrFiles = await Promise.all(adrFileTasks);
+		console.log({adrFiles});
+	}
+
+	async publishFile(file: TFile, spaceKey: string, parentPageId: string){
+		let text = await this.vault.cachedRead(file);
+		console.log({text})
+		const converter = new confluence.Converter();
+		text = text.replace(this.frontmatterRegex, "");
+		const adr = converter.makeHtml(text);
+
+		const pageTitle = file.basename
+		const searchParams = {type: "page", spaceKey, title: pageTitle, expand: "version"};
+		console.log({searchParams});
+		const contentByTitle = await this.confluenceClient.content.getContent(searchParams);
+		console.log({contentByTitle});
+
+		if (contentByTitle.size > 0)
+		{
+			const currentPage = contentByTitle.results[0];
+			console.log("Updating page", {currentPage})
+
+			const updateContentDetails = {
+				id: currentPage.id,
+				ancestors: [{id: parentPageId}],
+				version: {number: (currentPage?.version?.number ?? 1) + 1},
+				title: pageTitle,
+				type: "page",
+				body: {storage: {value: adr, representation: "storage"}}
+			};
+			console.log({updateContentDetails});
+			await this.confluenceClient.content.updateContent(updateContentDetails)
+		} else {
+			console.log("Creating page")
+			const creatingPageContent = {
+				space: {key: spaceKey},
+				ancestors: [{id: parentPageId}],
+				title: pageTitle,
+				type: "page",
+				body: {storage: {value: adr, representation: "storage"}}
+			};
+			console.log({creatingPageContent});
+			await this.confluenceClient.content.createContent(creatingPageContent);
+		}
 	}
 }
 
@@ -123,15 +178,58 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Confluence Domain')
+			.setDesc('Confluence Domain eg "https://mysite.atlassian.net"')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setPlaceholder('https://mysite.atlassian.net')
+			.setValue(this.plugin.settings.confluenceBaseUrl)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.confluenceBaseUrl = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+		.setName('Atlassian Username')
+		.setDesc('eg "username@domain.com"')
+		.addText(text => text
+		.setPlaceholder('username@domain.com')
+		.setValue(this.plugin.settings.atlassianUserName)
+		.onChange(async (value) => {
+			this.plugin.settings.atlassianUserName = value;
+			await this.plugin.saveSettings();
+		}));
+
+		new Setting(containerEl)
+		.setName('Atlassian API Token')
+		.setDesc('')
+		.addText(text => text
+		.setPlaceholder('')
+		.setValue(this.plugin.settings.atlassianApiToken)
+		.onChange(async (value) => {
+			this.plugin.settings.atlassianApiToken = value;
+			await this.plugin.saveSettings();
+		}));
+
+		new Setting(containerEl)
+		.setName('Confluence Parent Page ID')
+		.setDesc('Page ID to publish files under')
+		.addText(text => text
+		.setPlaceholder('23232345645')
+		.setValue(this.plugin.settings.confluenceParentId)
+		.onChange(async (value) => {
+			this.plugin.settings.confluenceParentId = value;
+			await this.plugin.saveSettings();
+		}));
+
+		new Setting(containerEl)
+		.setName('Folder to publish')
+		.setDesc('Publish all files except notes that are excluded using YAML Frontmatter')
+		.addText(text => text
+		.setPlaceholder('')
+		.setValue(this.plugin.settings.folderToPublish)
+		.onChange(async (value) => {
+			this.plugin.settings.folderToPublish = value;
+			await this.plugin.saveSettings();
+		}));
 	}
 }
