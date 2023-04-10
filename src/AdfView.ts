860bfb8e5cf1f094231abd2f4b4e7ae6bc91e7fd
Andrew McClenaghan <andrew.mcclenaghan@gmail.com>
2023-04-11 09:11:30 +1000

    feat(AdfPreview): Make local files render correctly in ADF Preview
    Loads the files via the `app://local` schema which enables them to be loaded as "external" files in the ADF Renderer component.

--- src/AdfView.ts
+++ src/AdfView.ts
@@ -1,5 +1,6 @@
 import {
 	EventRef,
+	FileSystemAdapter,
 	ItemView,
 	Menu,
 	Vault,
@@ -11,6 +12,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ReactRenderer } from "@atlaskit/renderer";
 import MdToADF from "./mdToADF";
+import { JSONDocNode } from "@atlaskit/editor-json-transformer";
+import { traverse, filter } from "@atlaskit/adf-utils/traverse";
+import { IntlProvider } from "react-intl-next";
 
 export const ADF_VIEW_TYPE = "AtlassianDocumentFormatView";
 
@@ -55,11 +59,58 @@ export default class AdfView extends ItemView {
 
 		let md = await this.app.vault.adapter.read(this.filePath);
 		const adf = this.mdToADF.parse(md);
-		ReactDOM.render(
+		const renderADF = this.convertMediaFilesToLocalPath(adf);
+
+		const locale = "en";
+		const messages = {
+			// Your localized messages
+		};
+
+		const intlProvider = React.createElement(
+			IntlProvider,
+			{
+				locale: locale,
+				messages: messages,
+			},
 			// @ts-ignore
-			React.createElement(ReactRenderer, { document: adf }),
-			container
+			React.createElement(ReactRenderer, { document: renderADF })
 		);
+
+		ReactDOM.render(intlProvider, container);
+	}
+
+	convertMediaFilesToLocalPath(adf: JSONDocNode): JSONDocNode {
+		const basePath = this.getBasePath();
+		return traverse(adf, {
+			media: (node, parent) => {
+				if (node?.attrs?.type === "file") {
+					console.log({ node });
+					const currentUrl = node?.attrs?.url as string;
+					const test = this.app.vault.adapter as FileSystemAdapter;
+					const vaultPath =
+						this.app.metadataCache.getFirstLinkpathDest(
+							currentUrl.replace("file://", ""),
+							this.filePath
+						);
+					if (!vaultPath) {
+						return false;
+					}
+					const path = test.getResourcePath(vaultPath.path);
+					console.log({ path });
+					node.attrs.type = "external";
+					node.attrs.url = path; // currentUrl.replace("file://", `file://${basePath}`);
+					return node;
+				}
+			},
+		}) as JSONDocNode;
+	}
+
+	getBasePath() {
+		let adapter = app.vault.adapter;
+		if (adapter instanceof FileSystemAdapter) {
+			return adapter.getBasePath();
+		}
+		throw new Error("No Path???");
 	}
 
 	async onClose() {
