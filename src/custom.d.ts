348f00e3d98bf6843f886310eeadf75ca86be1e3
Andrew McClenaghan <andrew.mcclenaghan@gmail.com>
2023-04-17 15:37:09 +1000

    feat(AdfEqual): Updates to make the ADF closer to what Confluence returns

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -12,3 +12,8 @@ declare module "mermaid_renderer.esbuild" {
 	const content: Buffer;
 	export default content;
 }
+
+declare module "sort-any" {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	export default function sortAny<T>(item: T): T;
+}
